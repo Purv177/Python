e8ef91639a29131a71adf8bcd76e4f2def64fa02
Purv177 <purvdoshi@gmail.com>
2024-10-09 20:28:42 +0530

    Add files via upload

---
+++ basic calc.py
@@ -0,0 +1,13 @@
+a=float(input("1st no.: "))
+b=float(input('2nd no.: '))
+c=input('arthematic operation(+,-,/,*,**): ')
+if c=='+':
+    print(a+b)
+elif c=='-':
+    print(a-b)
+elif c=='/':
+    print(a/b)
+elif c=='*':
+    print(a*b)
+else:
+    print('Enter valid operation')
