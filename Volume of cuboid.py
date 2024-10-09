fbbba91130e9186d80556edda166e3c98cf3270a
Purv177 <purvdoshi@gmail.com>
2024-10-09 20:29:18 +0530

    Add files via upload

---
+++ Volume of cuboid.py
@@ -0,0 +1,4 @@
+length = float(input("enter the length of the cuboid: "))
+width = float(input("enter the width of the cuboid: "))
+height = float(input("enter the height of the cuboid: "))
+print(f"the volume of cube is {length*width*height} cm^3")
