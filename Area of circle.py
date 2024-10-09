43eeb0f8183ac64bd8440ba5309ae31c349ee735
Purv177 <purvdoshi@gmail.com>
2024-10-09 20:24:56 +0530

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,5 @@
+import math
+
+radius = float(input("The radius of the circle is: "))
+area = math.pi * pow(radius,2)
+print(f"The area of the circle is: {round(area,2)} cm^2")
---
+++ Area of rectangle.py
@@ -0,0 +1,6 @@
+import math
+
+a = float(input("The length of side A is: "))
+b = float(input("The length of side B is: "))
+c = math.sqrt(pow(a,2) + pow(b,2))
+print(f"The length of side c is:{c}")
